f2b9cec899d2ee9a8b11eacb449a244d7375d4a6
RyanGuild <rdg27@pitt.edu>
2017-07-14 17:41:35 -0400

    v0.02

--- fileService.go
+++ fileService.go
@@ -1,14 +1,31 @@
 package teststorage
 
 import (
-	_ "golang.org/x/net/http2"
 	_ "cloud.google.com/go/storage"
 	_ "golang.org/x/oauth2"
 	_ "google.golang.org/appengine"
+	"net/http"
+	"google.golang.org/appengine"
+	"context"
+	"cloud.google.com/go/storage"
+	"io"
 )
 
 
 
 func main() {
+	http.HandleFunc("/", serveFile)
+	appengine.Main()
+}
+
+func serveFile (w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	client, _ := storage.NewClient(ctx)
+	rc, err := client.Bucket("teststatics").Object("html/main.html").NewReader(ctx)
+	if err != nil {
+		return
+	}
+	defer rc.Close()
+	io.Copy(w, rc)
 
 }
